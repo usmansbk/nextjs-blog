fcce0a091a1a1bf527648e2fdfb04695119ecbb0
usmansbk <usmansbk@gmail.com>
2023-02-22 20:45:52 +0100

    nit

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home({
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>I love coding!</p>
+        <p>I love to code!</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
